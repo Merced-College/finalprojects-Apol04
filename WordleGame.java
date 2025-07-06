78eeb8b97fbf4ef633abdc25a26a7198088c188f
Apol04 <angel.apo.cisneros@gmail.com>
2025-07-05 19:20:19 -0700

    Create WordleGame.java

---
+++ WordleGame.java
@@ -0,0 +1,13 @@
+import java.io;
+import java.util;
+
+public class WordleGame {
+
+  private static final int WORD_LENGTH = 5;
+  private static final int MAX_ATTEMPTS = 6;
+  private static final String WORD_LIST_FILE = "wordslist.txt";
+  private static final List<String> validWords = new ArrayList<>();
+  private static String targetWord;
+
+  
+}
